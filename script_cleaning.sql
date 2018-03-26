328436218918a8e582389fd72f6db0ce1a008c2a
Juan Pablo O <gnu.pablo@gmail.com>
2018-03-26 17:11:21 +0200

    Update consolidate subsidiaries table

--- script_cleaning.sql
+++ script_cleaning.sql
@@ -133,3 +133,79 @@ cib_PatApr17_v2_lab.tmp_for_delete);
 
 DELETE FROM consolidated_subsidiaries
 WHERE subs_bvd_id="DE2070000543";
+
+
+/*
+
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="TW04541302"
+WHERE bvd_id="CA35574NC";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="KY40148WB"
+WHERE bvd_id="CN9360001812";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="CN9363384623"
+WHERE bvd_id="CN9366690312";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="GB07524813"
+WHERE bvd_id="ESA48179196";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="SE5560003468"
+WHERE bvd_id="FR400782017";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="GBSC036219"
+WHERE bvd_id="GB01675285";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="DE2010000581"
+WHERE bvd_id="GB02180851";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="CN9360650064"
+WHERE bvd_id="IT00860340157";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="JP9020001031109"
+WHERE bvd_id="JP8030001014831";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="JP8010401050387"
+WHERE bvd_id="SE5560836461";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="TW84149961"
+WHERE bvd_id="TW16657967";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="CA30019NC"
+WHERE bvd_id="US202960116";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="US770118518"
+WHERE bvd_id="US251799439";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="US050315468"
+WHERE bvd_id="US411443470";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="US911144442"
+WHERE bvd_id="US770333710";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="GB06270876"
+WHERE bvd_id="US800318351";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="US270306875"
+WHERE bvd_id="US942586591";
+
+UPDATE consolidated_subsidiaries 
+SET bvd_id="CN9361203598"
+WHERE bvd_id="CN9360000761";
