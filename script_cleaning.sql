2c5a7a176faea4b58c77f61f988fb54c1cdcda64
Juan Pablo O <gnu.pablo@gmail.com>
2018-03-26 18:12:49 +0200

    Add comments to updates in the cleaning script

--- script_cleaning.sql
+++ script_cleaning.sql
@@ -103,6 +103,150 @@ WHERE bvd_id="USMCFA14";
 DELETE FROM companies
 WHERE bvd_id="USMCFA14";
 
+/*
+    HON HAI Precision Industry Co., Ltd.: TW04541302 
+    Smart Technologies Inc. : CA35574NC
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="TW04541302"
+WHERE bvd_id="CA35574NC";
+
+/*
+    Shunfeng International Clean Energy Limite: KY40148WB
+    Suntech Power Co., Ltd.: N9360001812
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="KY40148WB"
+WHERE bvd_id="CN9360001812";
+
+/*
+    China FAW Group Corporation (The First Automobile : CN9363384623
+    China Faw Co., Ltd.: CN9366690312
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="CN9363384623"
+WHERE bvd_id="CN9366690312";
+
+/*
+    Rolls-Royce Holdings PLC: B07524813
+    Industria De Turbo Propulsores SA: ESA48179196
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="GB07524813"
+WHERE bvd_id="ESA48179196";
+
+/*
+    Sandvik AB: SE5560003468
+    Varel Europe SA: FR400782017
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="SE5560003468"
+WHERE bvd_id="FR400782017";
+
+/*
+    John Wood Group P.L.C.: GBSC036219
+    Amec Foster Wheeler PLC: GB01675285
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="GBSC036219"
+WHERE bvd_id="GB01675285";
+
+/*
+    Siemens AG : DE2010000581
+    Siemens Industry Software Computational Dynamics Limited : GB02180851
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="DE2010000581"
+WHERE bvd_id="GB02180851";
+
+/*
+    China National Chemical Corporation : CN9360650064
+    Pirelli & C. S.P.A. : IT00860340157
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="CN9360650064"
+WHERE bvd_id="IT00860340157";
+
+/*
+    Nissan Motor CO Ltd : JP9020001031109
+    Calsonic Kansei Corporation: JP8030001014831
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="JP9020001031109"
+WHERE bvd_id="JP8030001014831";
+
+/*
+    Mitsubishi Heavy Industries Ltd: JP8010401050387
+    Unicarriers Europe AB:SE5560836461
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="JP8010401050387"
+WHERE bvd_id="SE5560836461";
+
+/*
+    Mediatek Inc.: TW84149961
+    Richtek Technology Corporation : TW16657967
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="TW84149961"
+WHERE bvd_id="TW16657967";
+
+/*
+    Novelion Therapeutics Inc. : CA30019NC
+    Aegerion Pharmaceuticals INC : US202960116
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="CA30019NC"
+WHERE bvd_id="US202960116";
+
+/*
+    Synaptics Incorporated : US770118518
+    Conexant Systems INC : US251799439
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="US770118518"
+WHERE bvd_id="US251799439";
+
+/*
+    Textron INC : US050315468
+    Arctic Cat INC : US411443470
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="US050315468"
+WHERE bvd_id="US411443470";
+
+/*
+    Microsoft Corp. : US911144442
+    Informatica Corp : US770333710
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="US911144442"
+WHERE bvd_id="US770333710";
+
+/*
+    Reckitt Benckiser Group PLC: GB06270876
+    Mead Johnson Nutrition Company : US800318351
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="GB06270876"
+WHERE bvd_id="US800318351";
+
+/*
+    M/A-Com Technology Solutions Holdings, Inc. : US270306875
+    Applied Micro Circuits Corp : US942586591
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="US270306875"
+WHERE bvd_id="US942586591";
+
+/*
+    Baosteel Group Co., Ltd. : CN9361203598
+    Baosteel Group Xinjiang Bayi Iron & Steel Co., Ltd. : CN9360000761
+*/
+UPDATE consolidated_subsidiaries 
+SET bvd_id="CN9361203598"
+WHERE bvd_id="CN9360000761";
+
 
 /*
     Volkswagen AG:	DE2070000543	
@@ -133,79 +277,3 @@ cib_PatApr17_v2_lab.tmp_for_delete);
 
 DELETE FROM consolidated_subsidiaries
 WHERE subs_bvd_id="DE2070000543";
-
-
-/*
-
-*/
-UPDATE consolidated_subsidiaries 
-SET bvd_id="TW04541302"
-WHERE bvd_id="CA35574NC";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="KY40148WB"
-WHERE bvd_id="CN9360001812";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="CN9363384623"
-WHERE bvd_id="CN9366690312";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="GB07524813"
-WHERE bvd_id="ESA48179196";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="SE5560003468"
-WHERE bvd_id="FR400782017";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="GBSC036219"
-WHERE bvd_id="GB01675285";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="DE2010000581"
-WHERE bvd_id="GB02180851";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="CN9360650064"
-WHERE bvd_id="IT00860340157";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="JP9020001031109"
-WHERE bvd_id="JP8030001014831";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="JP8010401050387"
-WHERE bvd_id="SE5560836461";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="TW84149961"
-WHERE bvd_id="TW16657967";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="CA30019NC"
-WHERE bvd_id="US202960116";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="US770118518"
-WHERE bvd_id="US251799439";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="US050315468"
-WHERE bvd_id="US411443470";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="US911144442"
-WHERE bvd_id="US770333710";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="GB06270876"
-WHERE bvd_id="US800318351";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="US270306875"
-WHERE bvd_id="US942586591";
-
-UPDATE consolidated_subsidiaries 
-SET bvd_id="CN9361203598"
-WHERE bvd_id="CN9360000761";
