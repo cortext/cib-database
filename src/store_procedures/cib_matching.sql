d726009a3cf59ab9e489c7cd800cd11c94e0da57
Juan Pablo O <gnu.pablo@gmail.com>
2019-05-29 16:36:53 +0200

    Define store procedur for matching

---
+++ src/store_procedures/cib_matching.sql
@@ -0,0 +1,12 @@
+DROP PROCEDURE IF EXISTS cib_matching;
+
+DELIMITER $$
+$$
+CREATE PROCEDURE cib_matching(IN type_match CHAR(20), IN `method` CHAR(20), 
+											  IN `filter` CHAR(20))
+		
+	
+BEGIN
+END$$
+DELIMITER ;
+
