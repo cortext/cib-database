6f322bfd5e6a3563af20e66585b904aeaf69460c
Juan Pablo O <gnu.pablo@gmail.com>
2019-06-06 12:19:10 +0200

    Define and add case clause for name for matching

--- src/store_procedures/cib_matching.sql
+++ src/store_procedures/cib_matching.sql
@@ -8,30 +8,29 @@ BEGIN
 	
 	DECLARE matching CHAR(50);
 	DECLARE name_for_where VARCHAR(255);
+    DECLARE name_clause VARCHAR(500);
+	DECLARE join_clause VARCHAR(500);
+	DECLARE condition_clause VARCHAR(20);
+	DECLARE where_clause VARCHAR(500);
 	SET @matching = CONCAT(name_match, '|', `method`);
 	SET @dt =  NOW();
 
-	SET @SQLText = 	CONCAT(' INSERT INTO cibmatch_log
-		SELECT new_bvd_id, original, CASE ', name_match, 
-		' WHEN original THEN A.original 
-		WHEN magerman THEN A.magerman
-		WHEN patstat_std THEN A.patstat_std
-		WHEN manual THEN A.manual
-		WHEN manual2 THEN A.manual2
-		WHEN manual3 THEN A.manual3
-		END AS name_for_match, 
-		B.doc_std_name_id, B.doc_std_name, 
-		@matching as match_type, @`filter`, @dt FROM global_ultimate_owners AS A
-		INNER JOIN cibmatch_patstat_applicants AS B
-		ON A.cnty_iso = B.iso_ctry
-		AND (CASE ', name_match, 
+	SET @name_clause = CONCAT('(CASE ', name_match, 
 	    		' WHEN original THEN A.original 
 	    		WHEN magerman THEN A.magerman
 	    		WHEN patstat_std THEN A.patstat_std
 	    		WHEN manual THEN A.manual
 	    		WHEN manual2 THEN A.manual2
 	    		WHEN manual3 THEN A.manual3
-			END) = B.doc_std_name;');    
+			    END)');
+	 	
+	SET @SQLText = 	CONCAT(' INSERT INTO cibmatch_log
+		SELECT new_bvd_id, original, ', @name_clause ,' AS name_for_match, 
+		B.doc_std_name_id, B.doc_std_name, 
+		@matching as match_type, @`filter`, @dt FROM global_ultimate_owners AS A
+		INNER JOIN cibmatch_patstat_applicants AS B
+		ON A.cnty_iso = B.iso_ctry
+		AND ', @name_clause ,' = B.doc_std_name;');    
 
  	PREPARE stmt FROM @SQLText;
     EXECUTE stmt;
