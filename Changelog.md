20b0f71b24aad03c6a3a35e315f5f30c834c71e5
Juan Pablo O <gnu.pablo@gmail.com>
2020-06-18 15:54:13 +0200

    Update changelog

---
+++ Changelog.md
@@ -0,0 +1,8 @@
+2.1 2020-19-06
+   - Add value in eur for net_income,  operating_revenue and total_assests in financial tables
+   - rename pl_before_taxes to net_income
+   - create two new store procedures for generating dynamically the financial tables and for pivot it as well
+   - change guo_bvd_id for firmreg_id
+   - add a new column for R&D investments in the financial table
+   - Modify the name in firms table
+   - Add new alternatives names into the firms_names table
--- src/sql/03 firm_financial_table.sql
+++ src/sql/03 firm_financial_table.sql
@@ -4,48 +4,20 @@
    new records 
 */
 
-INSERT INTO cib_firm_financial_data
-SELECT guo_orbis_id,
-       last_year - 2,
-       operating_revenue_two_years_previous_last_year,
-       total_assests_two_years_previous_last_year,
-       number_employees_two_years_previous_last_year,
-       pl_before_taxes_two_years_previous_last_year,
-       roe_using_pl_before_taxes_two_years_previous_last_year,
-       roa_using_pl_before_taxes_two_years_previous_last_year
-FROM   tmp_cluttered_financial_data; 
-
-INSERT INTO cib_firm_financial_data
-SELECT guo_orbis_id,
-       last_year - 1,
-       operating_revenue_previous_last_year,
-       total_assests_previous_last_year,
-       number_employees_previous_last_year,
-       pl_before_taxes_previous_last_year,
-       roe_using_pl_before_taxes_previous_last_year,
-       roa_using_pl_before_taxes_previous_last_year
-FROM   tmp_cluttered_financial_data; 
-
-INSERT INTO cib_firm_financial_data
-SELECT guo_orbis_id,
-       last_year,
-       operating_revenue_last_year,
-       total_assests_last_year,
-       number_employees_last_year,
-       pl_before_taxes_last_year,
-       roe_using_pl_before_taxes_last_year,
-       roa_using_pl_before_taxes_last_year
-FROM   tmp_cluttered_financial_data; 
+CALL pivot_financial_table('tmp_cib_firm_financial_data', 10);
 
 /* Cleaning steps for NULL values */
 
 SET @table_name = 'tmp_cib_firm_financial_data';
-CALL normalize_null(@table_name, 'operating_revenue');
-CALL normalize_null(@table_name, 'total_assests');
+CALL normalize_null(@table_name, 'operating_revenue_eur');
+CALL normalize_null(@table_name, 'operating_revenue_eur');
+CALL normalize_null(@table_name, 'net_income_eur');
+CALL normalize_null(@table_name, 'net_income_usd');
+CALL normalize_null(@table_name, 'total_assests_eur');
+CALL normalize_null(@table_name, 'total_assests_usd');
 CALL normalize_null(@table_name, 'number_employees');
-CALL normalize_null(@table_name, 'pl_before_taxes');
-CALL normalize_null(@table_name, 'roe_using_pl_before_taxes')
-CALL normalize_null(@table_name, 'roa_using_pl_before_taxes');
+CALL normalize_null(@table_name, 'roe_percent_before_taxes');
+CALL normalize_null(@table_name, 'roa_percent_before_taxes');
 
 /* 
    Insert into the final table (cib_firm_financial_data) in order to check 
